47306abc9cf2112327d9ca0289ca78667f8dbf28
SWETHA-DURAIRAJ <sec19ec020@sairamtap.edu.in>
2020-10-09 17:49:10 +0530

    web

---
+++ index.js
@@ -0,0 +1,18 @@
+function openWin() {
+    window.open("home.html");
+}
+function openap(){
+    window.open("aplitude.html");
+}
+function opencode(){
+    window.open("coding.html");
+}
+function openint(){
+    window.open("interview.html");
+}
+function openres(){
+    window.open("resume.html");
+}
+function openmock(){
+    window.open("mock.html")
+}
